0359a9bab2801ce12d482cab3d94764986b30a5f
devmansam <devmansam@protonmail.com>
2025-11-09 13:48:53 -0800

    adjusted max-width  of web inquiry form to 600px from 632px

--- web-inquiry-form.js
+++ web-inquiry-form.js
@@ -292,7 +292,7 @@ class WebInquiryForm extends HTMLElement {
         font-family: ${fontFamily};
         line-height: 1.6;
         color: ${textColor};
-        max-width: 632px;
+        max-width: 600px;
         margin: 0 auto;
         padding: 16px;
         font-size: ${fontSize};
